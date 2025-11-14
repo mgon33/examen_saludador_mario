40e330bbf34b9b8474e2d4199eff39edfd29d6a0
Mario <mgonzalmar33@educacion.navarra.es>
2025-11-14 17:52:17 +0100

    Cambio de saludo en mejoras

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo, soy Mario");
+        System.out.println("Hola, soy <tu nombre> desde la rama de mejoras!");
     }
 }
