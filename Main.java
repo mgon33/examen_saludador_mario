382651df055ef7cd9a450a712420313177f3173e
Mario <mgonzalmar33@educacion.navarra.es>
2025-11-14 17:49:17 +0100

    Cambio de saludo en main

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo, soy Mario");
+        System.out.println("Hola soy Mario desde la versión principal!");
+
     }
 }
