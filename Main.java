83db36b585953dc73bb2fd1bfa124ca251cbba4e
Mario <mgonzalmar33@educacion.navarra.es>
2025-11-14 17:44:31 +0100

    Añadido mensaje de despedida

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hola mundo, soy Mario");
         System.out.println("¡Que tengas un buen día!");
     }
 }
